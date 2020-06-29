49372f05bf7feaee0f8b528455be30ce3cfd14fa
unknown <nets.devs@gmail.com>
2020-06-29 13:12:02 +0300

    Laravel + Passport

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class MainController extends Controller
+{
+    //
+    public function register(Request $request){
+        $request->validate([
+            'name'=> 'required|string',
+            'email'=> 'required|string|email||unique:users',
+            'password'=> 'required|string|confirmed'
+        ]);
+        $user = new User([
+            'name'=> $request->name,
+            'email'=>$request->email,
+            'password'=> bcrypt($request->password)
+        ]);
+        $user->save();
+        return response()->json([
+            'message'=> 'Hey User a user was created'
+        ], 201);
+    }
+
+
+
+    public function login(Request $request){
+        $request->validate([
+            'email'=> 'required|string|email',
+            'remember_me'=> 'boolean'
+        ]);
+        $userCredentials = request(['email', 'password']);
+        if(!Auth::attempt($userCredentials))
+            return response()->json([
+               'message'=> 'Unauthorized'
+            ], 401);
+
+         $user = $request->user();
+         $tokenResult = $user->createToken('User Personal Access Token');
+         $token = $tokenResult->token;
+
+         if($request->remember_me){
+             $token->expires_at = Carbon::now()->addWeeks(2);
+         }
+        $token->save();
+        return response()->json([
+           'access_token' => $tokenResult->accessToken,
+            'token_type' => 'Bearer',
+            'expires_at' => Carbon::parse(
+                $tokenResult->token->expires_at
+            )->toDateString()
+        ]);
+
+    }
+
+    public function logout(Request $request){
+        $request->user()->token()->revoke();
+        return response()->json([
+            'message'=> 'hey you have been logged out Max!'
+        ]);
+    }
+
+    public function profile(Request $request){
+        return response()->json($request->user());
+    }
+
+}
